dc43f4cee6de933d071842f4a2f4fe0674328580
arioprima <arioprima2@gmail.com>
2024-07-23 16:50:19 +0700

    setting route

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,11 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "./satoshi.css";
-import { selectedRoutes } from "./utils/atomstates";
-import { useAtom } from "jotai";
 
 const RootComponent = () => {
-  const [appMode] = useAtom(selectedRoutes);
-  console.log(appMode, "appMode");
-
   return (
     <React.StrictMode>
-      <Router basename={`${appMode}`}>
+      <Router>
         <App />
       </Router>
     </React.StrictMode>
